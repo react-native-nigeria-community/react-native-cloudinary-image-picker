a791ccbcbba70bf0b8de01fe6a0022ebc8a62e0f
godswillokokon <godswillokokon3@gmail.com>
2020-04-27 01:00:00 +0100

    package App.js

--- App.js
+++ App.js
@@ -1,77 +1,57 @@
-import React, { useState } from 'react'
+
+import React from 'react'
 import {
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
   TouchableOpacity,
-  Image,
-  Alert
 } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
-import Index from './src/index'
 
+const CloudinaryImagePicker = (props) => {
+  const { cloudName, uploadPreset, url, design, } = props;
 
-const App = ({ responseData }) => {
-  console.log(responseData, "out")
+  const selectPhotoTapped = () => {
+    const options = {
+      title: 'Select Photo',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        props.onError('User cancelled image picker')
+      } else if (response.error) {
+        props.onError('ImagePicker Error: ', response.error)
+      } else {
+        const source = {
+          uri: response.uri,
+          type: response.type,
+          name: response.fileName,
+        }
+        cloudinaryUpload(source)
+      }
+    });
+  }
+  const cloudinaryUpload = (photo) => {
+    const data = new FormData()
+    data.append('file', photo)
+    data.append('upload_preset', uploadPreset)
+    data.append("cloud_name", cloudName)
+    fetch(url, {
+      method: "post",
+      body: data
+    })
+      .then(res => res.json())
+      .then(data => {
+        props.onSuccess(data)
+      })
+      .catch(err => {
+        props.onError(err)
+      })
+  }
   return (
-    <View>
-      <Index cloudName={"ogcodes"} uploadPreset={"ogcodes"} url={"https://api.cloudinary.com/v1_1/ogcodes/upload"} design={styles.uploadButton} >
-        <Text style={styles.uploadButtonText}>
-          Upload
-          </Text>
-
-      </Index>
-
-    </View>
+    <TouchableOpacity onPress={selectPhotoTapped} style={design}>
+      {props.children}
+    </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    backgroundColor: '#fe5b29',
-    height: Dimensions.get('window').height
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
-  uploadContainer: {
-    backgroundColor: '#f6f5f8',
-    borderTopLeftRadius: 45,
-    borderTopRightRadius: 45,
-    position: 'absolute',
-    bottom: 0,
-    width: Dimensions.get('window').width,
-    height: 200,
-  },
-  uploadContainerTitle: {
-    alignSelf: 'center',
-    fontSize: 25,
-    margin: 20,
-    fontFamily: 'Roboto'
-  },
-  uploadButton: {
-    borderRadius: 16,
-    alignSelf: 'center',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 7,
-      height: 5,
-    },
-    shadowOpacity: 1.58,
-    shadowRadius: 9,
-    elevation: 4,
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#fe5b29',
-    width: Dimensions.get('window').width - 60,
-    alignItems: 'center'
-  },
-  uploadButtonText: {
-    color: '#f6f5f8',
-    fontSize: 20,
-    fontFamily: 'Roboto'
-  }
-});
-export default App;
+export default CloudinaryImagePicker;
